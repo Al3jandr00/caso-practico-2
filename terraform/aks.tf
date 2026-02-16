b2e89070676945eadc8c08dadd15be61e47fa73c
Al3jandr00 <92246889+Al3jandr00@users.noreply.github.com>
2026-02-16 16:37:55 +0100

    Update aks.tf

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -1,29 +1,30 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-cp2-af"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "akscp2af"
+name = "aks-cp2-af"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+dns_prefix = "akscp2af"
 
-  default_node_pool {
-    name       = "nodepool1"
-    node_count = 1
-    vm_size    = "Standard_B2s"
-  }
+default_node_pool {
+name = "nodepool1"
+node_count = 1
+vm_size = "Standard_B2s"
+}
 
-  identity {
-    type = "SystemAssigned"
-  }
+identity {
+type = "SystemAssigned"
+}
 
-  network_profile {
-    network_plugin = "kubenet"
-  }
+network_profile {
+network_plugin = "kubenet"
+}
 
-  tags = {
-    project = "caso-practico-2"
-  }
-  resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  }
+tags = {
+project = "caso-practico-2"
+}
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+scope = azurerm_container_registry.acr.id
+role_definition_name = "AcrPull"
+principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
